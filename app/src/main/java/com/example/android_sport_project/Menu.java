6c4154bb829f0b1e9155b44ed25a14b9cf3d8ed6
Даниил Гладков <vip.daniil9904@mail.ru>
2020-04-20 21:35:49 +0300

    Splash screen add

---
+++ app/src/main/java/com/example/android_sport_project/Menu.java
@@ -0,0 +1,15 @@
+package com.example.android_sport_project;
+
+import android.os.Bundle;
+
+import android.app.Activity;
+
+
+public class Menu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_layout);
+    }
+}
