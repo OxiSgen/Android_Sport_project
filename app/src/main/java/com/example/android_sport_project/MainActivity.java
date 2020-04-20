25e1cef9b7bb7ad4ef2d557e514db2ed582899fe
Даниил Гладков <vip.daniil9904@mail.ru>
2020-04-20 21:10:03 +0300

    Splash screen add

--- app/src/main/java/com/example/android_sport_project/MainActivity.java
+++ app/src/main/java/com/example/android_sport_project/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.android_sport_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
+
+
+        Thread splash_time = new Thread() {
+            public void run() {
+                try {
+                    //Целое значение время отображения картинки:
+                    int SplashTimer = 0;
+                    //Запускаем цикл длиной в 3 секунды:
+                    while (SplashTimer < 3000) {
+                        sleep(50);
+                        SplashTimer = SplashTimer + 100;
+                    }
+                    ;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    //Закрываем activity:
+                    finish();
+                    startActivity(new Intent(MainActivity.this, Menu.class));
+                }
+            }
+        };
+        splash_time.start();
+
     }
 }
