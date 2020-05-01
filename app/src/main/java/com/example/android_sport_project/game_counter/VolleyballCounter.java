cb5545d9c67fc89d17876c7f2931f6f8c78ae22c
Nikita <nicromanov3000@gmail.com>
2020-05-02 01:10:34 +0300

    Add TimeCounter, refactor data Model.

--- app/src/main/java/com/example/android_sport_project/game_counter/VolleyballCounter.java
+++ app/src/main/java/com/example/android_sport_project/game_counter/VolleyballCounter.java
@@ -3,11 +3,13 @@ package com.example.android_sport_project.game_counter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.android_sport_project.R;
+import com.example.android_sport_project.model.SportGame;
 
 public class VolleyballCounter extends AppCompatActivity {
     private TextView counterFirstTeam;
@@ -15,14 +17,16 @@ public class VolleyballCounter extends AppCompatActivity {
     private TextView matchNameView;
     private TextView firstTeamNameView;
     private TextView secondTeamNameView;
-    public static String matchName;
-    public static String firstTeamName;
-    public static String secondTeamName;
+    private TextView gameTimer;
     private int firstTeamCounter;
     private int secondTeamCounter;
     private int LIMIT;
     private int MIN;
+    private String time;
+    private int seconds;
+    private boolean running;
 
+    public static SportGame volleyballGame = new SportGame();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,16 +35,21 @@ public class VolleyballCounter extends AppCompatActivity {
         secondTeamCounter = 0;
         LIMIT = 25;
         MIN = 0;
+        running = true;
+
         counterFirstTeam = (TextView) findViewById(R.id.FirstTeamCounter);
         counterSecondTeam = (TextView) findViewById(R.id.SecondTeamCounter);
         matchNameView = (TextView) findViewById(R.id.MatchName);
         firstTeamNameView = (TextView) findViewById(R.id.TeamOneName);
         secondTeamNameView = (TextView) findViewById(R.id.TeamTwoName);
+        gameTimer = (TextView) findViewById(R.id.GameTimer);
 
-        matchNameView.setText(matchName);
-        firstTeamNameView.setText(firstTeamName);
-        secondTeamNameView.setText(secondTeamName);
 
+        matchNameView.setText(volleyballGame.getMatchName());
+        firstTeamNameView.setText(volleyballGame.getFirstTeamName());
+        secondTeamNameView.setText(volleyballGame.getSecondTeamName());
+
+        timerGameRun();
         Button addOnePointToFirstTeam = (Button) findViewById(R.id.AddOnePointToFirstTeam);
         addOnePointToFirstTeam.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -81,15 +90,48 @@ public class VolleyballCounter extends AppCompatActivity {
             }
         });
 
+        Button pauseGame = (Button) findViewById(R.id.PauseGame);
+        pauseGame.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                running=!running;
+            }
+        });
+
         Button endGame = (Button) findViewById(R.id.EndMatch);
         endGame.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                running=false;
+            }
+        });
 
+
+    }
+
+    private void timerGameRun(){
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secon = seconds%60;
+
+                time = String.format("%d:%02d:%02d", hours, minutes, secon);
+                gameTimer.setText(time);
+                if(running){
+                    seconds++;
+                }
+                 handler.postDelayed(this, 1000);
             }
         });
     }
 
-    public void OnClick(View view) {
-
+    private void saveGame(){
+        VolleyballCounter.volleyballGame.setFirstTeamCount(firstTeamCounter);
+        VolleyballCounter.volleyballGame.setSecondTeamCount(secondTeamCounter);
+        VolleyballCounter.volleyballGame.setGameType(1);//Для волейболла тип 1
+        VolleyballCounter.volleyballGame.setGameTime(time);
+        //Тут будет кусок про пихание в JSON
     }
+
 }
---
+++ app/src/main/java/com/example/android_sport_project/helpers/JsonHelper.java
@@ -0,0 +1,16 @@
+package com.example.android_sport_project.helpers;
+
+import android.content.Context;
+
+import com.example.android_sport_project.model.SportGame;
+import com.google.gson.Gson;
+
+import java.util.List;
+
+public class JsonHelper {
+    static boolean exportToJSON(Context context, List<SportGame> dataList){
+
+        Gson gson = new Gson();
+        return false;
+    }
+}
