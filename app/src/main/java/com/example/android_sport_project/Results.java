c7d8528910712c004e5a201ab54543630660d460
Nikita Romanov <nicromanov3000@gmail.com>
2020-05-02 18:23:15 +0300

    Update Results.java

--- app/src/main/java/com/example/android_sport_project/Results.java
+++ app/src/main/java/com/example/android_sport_project/Results.java
@@ -10,10 +10,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class Results extends AppCompatActivity {
-
+private TextView result;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_table);
+        result = findViewById(R.id.ResultView);
+        String jsonResult = DataHelperSave.read(this);
+        result.setText(jsonResult);
+
+/*      Получение данных из JSON для парсинга --For Danya
+        Gson gson = new Gson();
+        JsonHelper.DataItem dataItem = gson.fromJson(DataHelperSave.read(this), JsonHelper.DataItem.class);
+        List<SportGame> sportGames = dataItem.getSportGames();*/
     }
 }
