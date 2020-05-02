c299e2d24f40d8c4537dd3ae2feed253bb4fd8fa
Nikita Romanov <nicromanov3000@gmail.com>
2020-05-02 18:40:25 +0300

    Update Results.java

--- app/src/main/java/com/example/android_sport_project/Results.java
+++ app/src/main/java/com/example/android_sport_project/Results.java
@@ -1,19 +1,26 @@
 package com.example.android_sport_project;
 
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import com.example.android_sport_project.helpers.DataHelperSave;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 public class Results extends AppCompatActivity {
-private TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(getWindow().FEATURE_NO_TITLE);
         setContentView(R.layout.activity_result_table);
-      /*   result = findViewById(R.id.ResultView);
+        // ConstraintLayout v = (ConstraintLayout) findViewById(R.id.result);
+        /*   result = findViewById(R.id.ResultView);
         String jsonResult = DataHelperSave.read(this);
         result.setText(jsonResult);
 
@@ -21,5 +28,13 @@ private TextView result;
         Gson gson = new Gson();
         JsonHelper.DataItem dataItem = gson.fromJson(DataHelperSave.read(this), JsonHelper.DataItem.class);
         List<SportGame> sportGames = dataItem.getSportGames();*/
+        
+
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int screenWidth = displaymetrics.widthPixels;
+        int screenHeight = displaymetrics.heightPixels;
+        getWindow().setBackgroundDrawableResource(R.drawable.kruglie_ugli);
+        getWindow().setLayout((int)(screenWidth/1.15),(int)(screenHeight/1.15));
     }
 }
