2162e01822d6405f0d457d61f5fd91b6445576b8
Nikita <nicromanov3000@gmail.com>
2020-05-02 18:54:39 +0300

    fixSomeShit

--- app/src/main/java/com/example/android_sport_project/Results.java
+++ app/src/main/java/com/example/android_sport_project/Results.java
@@ -6,13 +6,15 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
 import com.example.android_sport_project.helpers.DataHelperSave;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class Results extends AppCompatActivity {
-
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +22,21 @@ public class Results extends AppCompatActivity {
         supportRequestWindowFeature(getWindow().FEATURE_NO_TITLE);
         setContentView(R.layout.activity_result_table);
         // ConstraintLayout v = (ConstraintLayout) findViewById(R.id.result);
-        /*   result = findViewById(R.id.ResultView);
+        result = findViewById(R.id.ResultView);
         String jsonResult = DataHelperSave.read(this);
         result.setText(jsonResult);
 
-        Получение данных из JSON для парсинга --For Danya
+       /* Получение данных из JSON для парсинга --For Danya
         Gson gson = new Gson();
         JsonHelper.DataItem dataItem = gson.fromJson(DataHelperSave.read(this), JsonHelper.DataItem.class);
         List<SportGame> sportGames = dataItem.getSportGames();*/
-        
+
 
         DisplayMetrics displaymetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
         int screenWidth = displaymetrics.widthPixels;
         int screenHeight = displaymetrics.heightPixels;
         getWindow().setBackgroundDrawableResource(R.drawable.kruglie_ugli);
-        getWindow().setLayout((int)(screenWidth/1.15),(int)(screenHeight/1.15));
+        getWindow().setLayout((int) (screenWidth / 1.15), (int) (screenHeight / 1.15));
     }
 }
