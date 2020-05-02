027ef840b9870b9a9b31293fbbcb3c296ac7f390
Nikita Romanov <nicromanov3000@gmail.com>
2020-05-02 18:28:49 +0300

    Update Results.java

--- app/src/main/java/com/example/android_sport_project/Results.java
+++ app/src/main/java/com/example/android_sport_project/Results.java
@@ -23,11 +23,10 @@ private TextView result;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_table);
-        result = findViewById(R.id.ResultView);
+      /*   result = findViewById(R.id.ResultView);
         String jsonResult = DataHelperSave.read(this);
         result.setText(jsonResult);
-
-/*      Получение данных из JSON для парсинга --For Danya
+        Получение данных из JSON для парсинга --For Danya
         Gson gson = new Gson();
         JsonHelper.DataItem dataItem = gson.fromJson(DataHelperSave.read(this), JsonHelper.DataItem.class);
         List<SportGame> sportGames = dataItem.getSportGames();*/
