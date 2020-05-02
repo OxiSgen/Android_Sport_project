9c0384ed3f05dcdddb3ce06bd5a36f2443a8582b
Nikita Romanov <nicromanov3000@gmail.com>
2020-05-02 18:39:04 +0300

    Update Results.java

--- app/src/main/java/com/example/android_sport_project/Results.java
+++ app/src/main/java/com/example/android_sport_project/Results.java
@@ -6,6 +6,7 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import com.example.android_sport_project.helpers.DataHelperSave;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -19,6 +20,15 @@ public class Results extends AppCompatActivity {
         supportRequestWindowFeature(getWindow().FEATURE_NO_TITLE);
         setContentView(R.layout.activity_result_table);
         // ConstraintLayout v = (ConstraintLayout) findViewById(R.id.result);
+        /*   result = findViewById(R.id.ResultView);
+        String jsonResult = DataHelperSave.read(this);
+        result.setText(jsonResult);
+
+        Получение данных из JSON для парсинга --For Danya
+        Gson gson = new Gson();
+        JsonHelper.DataItem dataItem = gson.fromJson(DataHelperSave.read(this), JsonHelper.DataItem.class);
+        List<SportGame> sportGames = dataItem.getSportGames();*/
+        
 
         DisplayMetrics displaymetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
