27591d580c66987ff15291114f919a88a47cd4f2
Alexandr Panov <grimasa904@gmail.com>
2020-05-03 23:54:07 +0300

    PIZDA

---
+++ app/src/main/java/com/example/android_sport_project/Results.java
@@ -0,0 +1,15 @@
+package com.example.android_sport_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Results extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_table);
+
+    }
+}
