5b623db653bf097d3d8084a1f6395bc80ce2f3e5
Даниил Гладков <vip.daniil9904@mail.ru>
2020-05-09 13:36:46 +0300

    result table ver. 0.2

--- app/src/main/java/com/example/android_sport_project/Results.java
+++ app/src/main/java/com/example/android_sport_project/Results.java
@@ -9,12 +9,21 @@ import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.example.android_sport_project.helpers.DataHelperSave;
+import com.example.android_sport_project.helpers.JsonHelper;
+import com.example.android_sport_project.model.SportGame;
+import com.google.gson.Gson;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class Results extends AppCompatActivity {
-    TextView result;
+    //TextView result;
+    private RecyclerView recyclerView;
+    private ScoreAdapter adapter = new ScoreAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +31,15 @@ public class Results extends AppCompatActivity {
         supportRequestWindowFeature(getWindow().FEATURE_NO_TITLE);
         setContentView(R.layout.activity_result_table);
         // ConstraintLayout v = (ConstraintLayout) findViewById(R.id.result);
-        result = findViewById(R.id.ResultView);
-        String jsonResult = DataHelperSave.read(this);
-        result.setText(jsonResult);
+        //result = findViewById(R.id.ResultView);
+       // String jsonResult = DataHelperSave.read(this);
+       // result.setText(jsonResult);
 
-       /* Получение данных из JSON для парсинга --For Danya
-        Gson gson = new Gson();
-        JsonHelper.DataItem dataItem = gson.fromJson(DataHelperSave.read(this), JsonHelper.DataItem.class);
-        List<SportGame> sportGames = dataItem.getSportGames();*/
+        // данных из JSON для парсинга --For Danya
 
+        recyclerView = findViewById(R.id.rvMain);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         DisplayMetrics displaymetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
@@ -38,5 +47,19 @@ public class Results extends AppCompatActivity {
         int screenHeight = displaymetrics.heightPixels;
         getWindow().setBackgroundDrawableResource(R.drawable.kruglie_ugli);
         getWindow().setLayout((int) (screenWidth / 1.15), (int) (screenHeight / 1.15));
+
+
+        initData();
+    }
+    private void initData() {
+        Gson gson = new Gson();
+        JsonHelper.DataItem dataItem = gson.fromJson(DataHelperSave.read(this), JsonHelper.DataItem.class);
+        List<SportGame> sportGames = dataItem.getSportGames();
+        for(int i = 0; i < sportGames.size(); i++) {
+            String Score = (sportGames.get(i).getFirstTeamCount()) + ":" + (sportGames.get(i).getSecondTeamCount());
+            adapter.addScore(new Score(sportGames.get(i).getGameType(), sportGames.get(i).getFirstTeamName(), Score , sportGames.get(i).getSecondTeamName(), sportGames.get(i).getGameDate()));
+        }
+
     }
 }
+
