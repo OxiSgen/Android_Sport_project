b398e2a3c86a7a7743e780323d4ae315a5b7d60e
Даниил Гладков <vip.daniil9904@mail.ru>
2020-05-26 00:12:02 +0300

    delete button blocks

--- app/src/main/java/com/example/android_sport_project/game_counter/VolleyballCounter.java
+++ app/src/main/java/com/example/android_sport_project/game_counter/VolleyballCounter.java
@@ -134,18 +134,18 @@ public class VolleyballCounter extends AppCompatActivity implements Counter {
                                              stopStartTime();
                                              if (running) {
                                                  pauseGame.setImageResource(R.drawable.ic_stop_black_24dp);
-                                                 addOnePointToFirstTeam.setEnabled(true);
+                                                 /* addOnePointToFirstTeam.setEnabled(true);
                                                  addOnePointToSecondTeam.setEnabled(true);
                                                  removeOnePointFromFirstTeam.setEnabled(true);
                                                  removeOnePointFromSecondTeam.setEnabled(true);
-                                                 endGame.setEnabled(true);
+                                                 endGame.setEnabled(true); */
                                              } else {
                                                  pauseGame.setImageResource(R.drawable.ic_play_arrow_black_24dp);
-                                                 addOnePointToFirstTeam.setEnabled(false);
+                                               /*   addOnePointToFirstTeam.setEnabled(false);
                                                  addOnePointToSecondTeam.setEnabled(false);
                                                  removeOnePointFromFirstTeam.setEnabled(false);
                                                  removeOnePointFromSecondTeam.setEnabled(false);
-                                                 endGame.setEnabled(false);
+                                                 endGame.setEnabled(false); */
                                              }
                                          }
                                      }
